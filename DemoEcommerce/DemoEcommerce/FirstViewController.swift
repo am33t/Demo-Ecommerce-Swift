badc1cafaad2a09ee99a2d43d16d675f2b31da6e
Amit Tandel <amittandel8@gmail.com>
2017-12-19 23:46:49 +0530

    Initial Commit. Installed pods.

---
+++ DemoEcommerce/DemoEcommerce/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  DemoEcommerce
+//
+//  Created by Amit Tandel on 19/12/17.
+//  Copyright © 2017 Amit Tandel. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ DemoEcommerce/DemoEcommerce/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  DemoEcommerce
+//
+//  Created by Amit Tandel on 19/12/17.
+//  Copyright © 2017 Amit Tandel. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
